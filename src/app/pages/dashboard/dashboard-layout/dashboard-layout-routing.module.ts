4cf100f0962fd5b65d3d95ea1e09cd77f1351a2b
Srishti <Srishti@cozentus.com>
2022-04-11 16:52:11 +0530

    Deleted un-necessary files
    ams-service deleted
    requests module deleted

--- src/app/pages/dashboard/dashboard-layout/dashboard-layout-routing.module.ts
+++ src/app/pages/dashboard/dashboard-layout/dashboard-layout-routing.module.ts
@@ -3,10 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardLayoutComponent } from './dashboard-layout.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardLayoutComponent, 
+  { path: '', component: DashboardLayoutComponent,
   children:[
     { path: '', redirectTo:'raiserequest',pathMatch:'full'},
-    { path: 'requests', loadChildren: ()=> import('../requests/requests.module').then((m)=> m.RequestsModule)},
     { path: 'raiserequest', loadChildren:()=>import('../raiserequest/raiserequest.module').then((m)=>m.RaiserequestModule)},
     {path:'raiserequest',loadChildren:()=>import('../raiserequest/raiserequest.module').then((m)=>m.RaiserequestModule)},
     { path: 'dispatch', loadChildren: () => import('../dispatch-information/dispatch-information.module').then(m => m.DispatchInformationModule) }
